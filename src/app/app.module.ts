caf95ea50b469ebe5690943f7fac0fedb3f58132
designjrc <info@designjrc.com>
2018-07-10 21:36:52 -0300

    Chewan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './modulos/angular-material/angular-material.module';
+import { AppRoutingModule } from './/app-routing.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
